2d3e8a5fa186c1ee96732391e5323d3547daec14
shieldTM <hafez.janjan@gmail.com>
2016-04-04 13:48:44 +0430

    Update del.lua

--- plugins/del.lua
+++ plugins/del.lua
@@ -1,5 +1,3 @@
-do
-
 local function get_message_callback(extra, success, result)
   if result.to.peer_type == 'channel' then
     if our_id == result.from.peer_id then
@@ -12,25 +10,23 @@ local function get_message_callback(extra, success, result)
       end
     end
   else
-    send_msg(extra, "", ok_cb, false)
+    send_msg(extra, "You're not in a group", ok_cb, false)
     return
   end
 end
 local function run(msg, matches)
-  if matches[1] == "del" and msg.reply_id then
+  if matches[1] == "del" and is_momod(msg) and msg.reply_id then
     msgr = get_message(msg.reply_id,get_message_callback, get_receiver(msg))
   end
 end
-
 return {
-   description = "del-msg by reply.",
+   description = "kick by reply.",
    usage = {
    },
    patterns = {
-      "^[!/#](del)$"
+      "^[#/!](del)$"
    },
    run = run,
-   moderated = true,
    hide = true
-
-}
+   
+}  
