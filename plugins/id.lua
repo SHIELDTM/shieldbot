3fa27f132f0c9be2cbb6542fa51361f1dd3b2be2
shieldTM <hafez.janjan@gmail.com>
2016-04-06 21:09:56 +0430

    Create id.lua

---
+++ plugins/id.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "👤Name : "..(msg.from.print_name or '').."\n🅰first name : "..(msg.from.first_name or '').."\n🅱last name : "..(msg.from.last_name or '').."\n\n🆔ID : "..msg.from.id.."\n🌐username : @"..msg.from.username.."\n\n🆔SuperGroup ID : "..msg.to.id.."\n👥SuperGroup name : "..msg.to.title.."\n@shieldTM"
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[!/#][Ii][Dd]$",
+  },
+  run = run
+}
+end
