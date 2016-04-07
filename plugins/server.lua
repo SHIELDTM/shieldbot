26e8d106694df71d68151397f3e50b2d4625d2fe
shieldTM <hafez.janjan@gmail.com>
2016-04-07 18:06:12 +0430

    Create server.lua

---
+++ plugins/server.lua
@@ -0,0 +1,20 @@
+do
+    
+function run(msg, matches)
+  if matches[1] == "reboot" and is_sudo(msg) then
+        local s = io.popen("sh ./data/upstart.sh") 
+        return ( s:read("*a") ) 
+  elseif matches[1]:lower() == "serverinfo" and is_sudo(msg) then
+     local f = io.popen("sh ./data/com/cmd.sh") 
+     return ( f:read("*a") ) 
+  end
+end
+return {
+  patterns = {
+   -- "^(reboot)",
+    "^([Ss][Ee][Rr][Vv][Ee][Rr][Ii][Nn][Ff][Oo])"
+    "^[!/#]([Ss][Ee][Rr][Vv][Ee][Rr][Ii][Nn][Ff][Oo])"
+  },
+  run = run
+}
+end
