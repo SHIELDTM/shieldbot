29746454facb07e2d48bb2e8b07c71712eae2d2c
shieldTM <hafez.janjan@gmail.com>
2016-04-08 09:11:49 +0430

    Update get.lua

--- plugins/get.lua
+++ plugins/get.lua
@@ -11,7 +11,7 @@ local function get_value(msg, var_name)
     if not value then
       return
     else
-      return var_name..':\n'..value
+      return '..value
     end
   end
 end
@@ -31,7 +31,8 @@ end
 
 return {
   patterns = {
-    "^([#!/]get) (.+)$"
+    "^([#!/]get) (.+)$"‚
+    "^([#!/])(.+)$"
   },
   run = run
 }
