dc6f3c0df297e1a82f363a017e7b0c864e7900fb
shieldTM <hafez.janjan@gmail.com>
2016-04-08 16:24:49 +0430

    Update stick.lua

--- plugins/stick.lua
+++ plugins/stick.lua
@@ -1,27 +1,34 @@
-do local function run(msg, matches) local text = matches[1]
-  local b = 1
-  while b ~= 0 do
-    textc = text:trim()
-    text,b = text:gsub(' ','.')
-    
-    
-  if msg.to.type == 'user' then
-      return nil
-      else
-  local url = 
-"http://latex.codecogs.com/png.download?".."\\dpi{800}%20\\LARGE%20"..textc
-  local receiver = get_receiver(msg)
-  local file = download_to_file(url,'text.webp')
-      send_document('channel#id'..msg.to.id, file, ok_cb , false) end 
+
+do
+
+local function send_title(cb_extra, success, result)
+  if success then
+  send_msg(cb_extra[1], cb_extra[2], ok_cb, false)
+ end
 end
-  end return {
-  description = "text to voice",
+
+local function run(msg, matches)
+    
+  local eq = URL.escape(matches[2])
+local size = URL.escape(matches[1])
+  local url = "http://latex.codecogs.com/png.download?"
+    .."\\dpi{"..size.."}%20\\LARGE%20"..eq
+
+  local receiver = get_receiver(msg)
+  local title = "Edit LaTeX on www.codecogs.com/eqnedit.php?latex="..eq
+  local file = download_to_file(url,'shield.webp')
+      send_document('channel#id'..msg.to.id, file, ok_cb , false)
+end
+
+return {
+  description = "Convert LaTeX equation to image",
   usage = {
-    "!voice [text]"
+    "!tex [equation]: Convert LaTeX equation to image"
   },
   patterns = {
-    "^[!/#]sticker +(.*)$",
+    "^[!/#]sticker (.+) (.+)$"
   },
   run = run
 }
+
 end
